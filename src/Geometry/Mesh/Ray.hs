9f2100dd84c70d523f751f55fb8bdf80a1c24619
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-11-02 00:28:43 +0100

    start working on the hitpoint

--- src/Geometry/Mesh/Ray.hs
+++ src/Geometry/Mesh/Ray.hs
@@ -2,20 +2,21 @@
 
 module Geometry.Mesh.Ray (
     Ray(Ray, origin, direction, near, far)
-  , Hittable(rayHits, rayHitsWithFast)
+  , Hittable(rayHits, rayHitsWithFast, rayHitsAt)
   ) where
 
-import Linear.V3(V3)
+import Geometry.Mesh.Base(P3)
 
 data Ray a = Ray {
-    origin :: V3 a
-  , direction :: V3 a
+    origin :: P3 a
+  , direction :: P3 a
   , near :: a
   , far :: a
   } deriving (Eq, Ord, Read, Show)
 
 class Hittable f where
     rayHits :: (Num a, Ord a) => Ray a -> f a -> Bool
+
     rayHitsWithFast :: (Num a, Ord a, Hittable fast) => Ray a -> (f a, fast a) -> Bool
     rayHitsWithFast ray ~(o, e) = rayHits ray e && rayHits ray o
     {-# MINIMAL rayHits #-}
