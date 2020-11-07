8d1db40b372f1b1746bedd640c20c97c58128394
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-11-07 13:10:12 +0100

    .obj parsing of face indexes

--- src/Geometry/Mesh/Parser/ObjParser.hs
+++ src/Geometry/Mesh/Parser/ObjParser.hs
@@ -2,16 +2,24 @@
 
 module Geometry.Mesh.Parser.ObjParser where
 
+import Data.List.NonEmpty(NonEmpty((:|)))
+
 import Geometry.Mesh.Internal(eolf, spaceFloating, toEndOfLine)
 
 import Linear.V3(V3(V3))
 
-import Text.Parsec(ParsecT, Stream, optional)
+import Text.Parsec(ParsecT, Stream, many, optional)
 import Text.Parsec.Char(char)
+import Text.Parsec.Number(int)
 
 vertex :: (Floating a, Stream s m Char) => ParsecT s u m (V3 a)
 vertex = char 'v' *> (V3 <$> go <*> go <*> go) <* optional go <* eolf
     where go = spaceFloating
+
+
+
+findex :: (Integral i, Stream s m Char) => ParsecT s u m (NonEmpty i)
+findex = (:|) <$> int <*> many (char '/' *> int)
 
 comment :: Stream s m Char => ParsecT s u m ()
 comment = char '#' *> toEndOfLine *> eolf
