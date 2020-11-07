88d6b1289446832533e54db99abc3c1882b3acc3
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-11-07 11:32:46 +0100

    start working on a .obj parser

--- src/Geometry/Mesh/Parser/ObjParser.hs
+++ src/Geometry/Mesh/Parser/ObjParser.hs
@@ -6,15 +6,12 @@ import Geometry.Mesh.Internal(eolf, spaceFloating, toEndOfLine)
 
 import Linear.V3(V3(V3))
 
--- import Text.Parsec.ByteString.Lazy()
--- import Text.Parsec.Prim
-import Text.Parsec(ParsecT, Stream)
+import Text.Parsec(ParsecT, Stream, optional)
 import Text.Parsec.Char(char)
--- import Text.Parsec.Combinator
-
 
 vertex :: (Floating a, Stream s m Char) => ParsecT s u m (V3 a)
-vertex = char 'v' *> (V3 <$> spaceFloating <*> spaceFloating <*> spaceFloating) <* eolf
+vertex = char 'v' *> (V3 <$> go <*> go <*> go) <* optional go <* eolf
+    where go = spaceFloating
 
 comment :: Stream s m Char => ParsecT s u m ()
 comment = char '#' *> toEndOfLine *> eolf
